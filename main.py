409aa75c580950537070206940a8f0b3fb570b99
Shubhayu Majumdar <majumdarshubhayu@gmail.com>
2021-06-09 03:36:14 +0530

    Revert "eheheheheheheheuheuehueheuheuheueheuheueh"

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from geoLocate_class import geoLocate
+import requests
+import time
+import json
+import config
+from pprint import pprint
+
+
+class geoLocate:
+    def __init__(self, location):
+        self.location = location
+        self.baseURL = "https://maps.googleapis.com/maps/api/geocode/json"
+        self.params = {
+            'address': self.location,
+            'key': config.api_key
+        }
+
+    def fetchCoordinates(self):
+        self.data = requests.get(self.baseURL, params=self.params).json()
+        pprint(self.data)
+
 
 if __name__ == '__main__':
-    location = "DARJEELING DISTRICT HOSPITAL (Government Hospital)"
+    location = "Techno Main Saltlake, Kolkata"
     dir = geoLocate(location)
-    coordinates = dir.fetchCoordinates()
-    print(coordinates)
+    dir.fetchCoordinates()
